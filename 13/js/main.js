2faf55ff563118e0704e4f30adba2c20231f651d
Keks <keks@htmlacademy.ru>
2025-11-27 13:18:13 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/main.js
@@ -0,0 +1,7 @@
+import {createPost} from "./posts.js";
+import { showPost } from "./showUsersPosts.js";
+import "./fullImageModalWindow.js";
+import "./formValidation.js"
+
+showPost();
+console.log(createPost);
